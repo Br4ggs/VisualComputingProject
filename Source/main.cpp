a26e92547263af01eca1566f7b6cb20db3542d68
Jelte Dirks <j.dirks@outlook.com>
2025-02-18 18:28:19 +0100

    intermediate code that is not working yet
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,6 +6,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <chrono>
 
 typedef struct Shader {
     int size;
@@ -31,45 +32,15 @@ int load_from_file(const char * const filename, ShaderFile* sf) {
     ifs.read(sf->content, size);
     ifs.close();
     sf->content[size] = '\0';
-    std::cout << sf->content;
 
     return 0;
 }
 
 int main()
 {
-    if (!glfwInit())
+    if (glfwInit() == GLFW_FALSE)
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLfloat vertices[] =
-    {
-        -1., -1.,  0., /* bottom left */
-         0.,  0.,  0., /* top */
-         1., -1.,  0., /* bottom right */
-    };
-
-    GLFWwindow* window = glfwCreateWindow(1000, 800, "VisualComputingProject", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); //enable vsync
-
-    int version = gladLoadGL();
-    if (version == 0)
-    {
-        std::cout << "Failed to initialize OpenGL context" << std::endl;
         return -1;
     }
 
@@ -86,38 +57,105 @@ int main()
         return 1;
     }
 
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "VisualComputingProject", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    int version = gladLoadGL();
+    if (version == 0) {
+        std::cout << "Glad could not bind OpenGL context\n";
+        return 1;
+    }
+
+    glfwSwapInterval(1); /* 1 = V-sync, 0 = ASAP */
+
+    if (version == 0)
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    GLfloat vertices[] =
+        {
+            -1., -1.,  0., 0., /* bottom left */
+            -0.8,  0.8,  0., 0., /* top left */
+            -0.8, 0.8,  0., 0., /* bottom right */
+            1.,  1.,  0., 0.  /* top right */
+        };
+    unsigned int triangle_elements[] = 
+        {
+            0, 1, 2, /* first triangle vertices */
+            0, 2, 3  /* second triangle vertices */
+        };
+
     glViewport(0, 0, 800, 800);
+
+    // === Creating shaders and attaching to program ===
+
+    GLuint shaderProgram = glCreateProgram(); /* should hold all shaders */
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertex_shader->content, NULL);
     glCompileShader(vertexShader);
+    glAttachShader(shaderProgram, vertexShader);
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragment_shader->content, NULL);
     glCompileShader(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+
+    glLinkProgram(shaderProgram); /* can delete compiled shaders after linking */
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    //vertex array and vertex buffer object to transfer vertex data from cpu to gpu
-    //ordering is important here!
-    GLuint vertexArray;
-    GLuint vertexBuffer;
-    glGenVertexArrays(1, &vertexArray);
-    glGenBuffers(1, &vertexBuffer);
 
-    glBindVertexArray(vertexArray);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    // === Setting up communication between CPU and GPU using buffers ===
 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint vertex_arrays; /* creates a placeholder object */
+    glGenVertexArrays(1, /* number of arrays */
+                      &vertex_arrays); /* placeholder for indices */
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    GLuint vertex_buffer; /* is going to hold all vertices that exist */
+    GLuint element_buffer; /* enables reuse of vertices by specifying which elements belong together */
+
+    /* create the buffers */
+    glGenBuffers(1, &vertex_buffer);
+    glGenBuffers(1, &element_buffer);
+
+    glBindVertexArray(vertex_arrays); /* set the current vertex array object */
+
+    glBindBuffer(GL_ARRAY_BUFFER, /* target to which to bind */
+                 vertex_buffer); /* name of buffer object */
+    glBufferData(GL_ARRAY_BUFFER, /* target buffer to set data in */
+                 sizeof(vertices), /* size in bytes of new data */
+                 vertices, /* new data */
+                 GL_STATIC_DRAW); /* usage pattern hint for memory layout */
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle_elements), triangle_elements, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, /* index of vertex attribute */
+                          3, /* number of components per attribute, for triangle this is 3 */
+                          GL_FLOAT, /* type of values in array */
+                          GL_FALSE, /* should not be normalized anymore */
+                          3 * sizeof(float), /* stride of the attributes */
+                          (void*)0); /* pointer to first vertex attribute */
+    glEnableVertexAttribArray(0); /* enable the array with index 0 (see above)*/
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     //unbind buffers
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -128,22 +166,62 @@ int main()
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
 
+    auto lastTime = std::chrono::high_resolution_clock::now();
+    int frameCount = 0;
+
+    int head = 0;
+    int size = 10;
+    int* sample = (int*)malloc(size * sizeof(int));
+
     while (!glfwWindowShouldClose(window))
     {
-        //draws triangle
-        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vertexArray);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        frameCount++;
 
-        //render
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        auto timeDelta = std::chrono::duration<double>(currentTime - lastTime).count();
+
+        if (timeDelta >= 1.0) // Every second
+        {
+            sample[head++] = frameCount;
+
+            if (head == size) {
+                head = 0;
+                for (int i = 0; i < size; ++i) {
+                    std::cout << sample[i] << " ";
+                }
+                std::cout << "\n";
+            }
+
+            frameCount = 0;
+            lastTime = currentTime;
+        }
+
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f); /* rgba for when colors are cleared */
+        glClear(GL_COLOR_BUFFER_BIT); /* clear the color buffer */
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertex_arrays);
+        glDrawElements(GL_TRIANGLES, /* primitive to render */
+                       6, /* number of elements to render */
+                       GL_UNSIGNED_INT, /* type of value in the 'indices' */
+                       0); /* byte offset into the bound buffer, or pointer to location if no buffer is bound */
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &vertexArray);
-    glDeleteBuffers(1, &vertexBuffer);
+    for (int i = 0; i < head; ++i) {
+        std::cout << sample[i] << " ";
+    }
+    std::cout << "\n";
+
+    free(sample);
+    free(vertex_shader->content);
+    free(vertex_shader);
+    free(fragment_shader->content);
+    free(fragment_shader);
+
+    glDeleteVertexArrays(1, &vertex_arrays);
+    glDeleteBuffers(1, &vertex_buffer);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
--- Source/shaders/simple.frag
+++ Source/shaders/simple.frag
@@ -2,6 +2,10 @@
 
 out vec4 FragColor;
 
+float sdf_sphere(vec3 position, float radius) {
+   return length(position) - r;
+}
+
 void main()
 {
    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
